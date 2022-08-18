49838e972a48f50c084d835290a35c45ba803a0f
Sonny <sonnyelvis@hotmail.co.uk>
2022-08-18 21:24:14 +0100

    Add draggable, more ui changes.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script>
+	import {draggable} from '@neodrag/svelte';
 	import {browser} from '$app/env'
 	import {presets} from '$lib/data/presets'
 	import {getStorage, setStorage, compareObjects} from '$lib/util/util';
@@ -55,9 +56,9 @@
 		for (let [key, value] of Object.entries(newValues)) {
     	    if (newValues.hasOwnProperty(key) && properties.hasOwnProperty(key)) {
 				if (properties[key] !== value) {
-            		properties[key] = value
+            		properties[key] = value;
 					if (scope === "global") {
-						updateValue(String(key), value)
+						updateValue(String(key), value);
 					}
 				}
 			}
@@ -70,7 +71,7 @@
 		sendMessage({message: "exit-options"}, "Exiting now.");
 		window.close();
 	}
-
+	
 	if (browser) {
 		getStorage("preset").then((storedPreset) => {activePreset = storedPreset ?? activePreset;});
 		chrome.runtime.sendMessage({
@@ -83,61 +84,65 @@
 			status = response.message;
 		})
 	}
+	// disabled = false;
 
 </script>
 
-<div class="flex flex-col w-full h-auto opacity-80 bg-gradient-to-tr from-orange-600 via-pink-600 to-purple-700 background-animate blur">
-<div class="flex flex-row children:(flex flex-col pl-2)">	
-	<div class="min-w-[50%] pl-5">
-		<h1 class="text">Presets:</h1>
-		{#each presets as preset, i}
-			<div class="flex flex-row items-center hover:(bg-gray-400/20)" on:click={() => {
-				setPreset(i);
-				updateValues(presets[activePreset].values, "global");}}>
-				<input class="" type="radio" name="activePreset" bind:group={activePreset} value={i}>
-				<span class="text">{preset.name}</span>
-			</div>
-		{/each}
-	</div>
-	<div class="min-w-[50%] mr-4">
-		<h1 class="text">Playback rate: {properties.playbackRate}x</h1>
-		<input class="m-2" type="range" min="0" max="2" step="0.05" {disabled} bind:value={properties.playbackRate} on:change={() => {updateValue("playbackRate", properties.playbackRate)}}>
-		<h1 class="text text-center">Pitch</h1>
-		<div class="flex flex-row children:min-w-1/2">
-			<div class="flex flex-col">
-			<h2 class="text">Semitone Shift: {properties.pitch}</h2>
-			<input class="m-2" type="range" min="-12" max="12" step="1" {disabled} bind:value={properties.pitch} on:change={() => {updateValue("pitch", properties.pitch)}}>
-			</div>
-			<div class="flex flex-col">
-			<h2 class="text">Wetness: {properties.pitchWet}</h2>
-			<input class="m-2" type="range" min="0" max="1" step="0.01" {disabled} bind:value={properties.pitchWet} on:change={() => {updateValue("pitchWet", properties.pitchWet)}}>
-			</div>
+<div class="flex flex-col w-[600px] opacity-80 bg-gradient-to-tr from-orange-600 via-pink-600 to-purple-700 background-animate blur">
+	<div class="flex flex-row children:(flex flex-col pl-2)">
+		<div class="min-w-[50%] pl-2 children:rounded-md" use:draggable>
+			<h1 class="text-center text big-text">Audio Presets:</h1>
+			{#each presets as preset, i}
+				<div class={(activePreset === i ? "bg-green-300/20" : "") + " flex flex-row items-center hover:(bg-green-300/20)"} on:click={() => {
+					setPreset(i);
+					updateValues(presets[activePreset].values, "global");}}>
+					<input class="ml-2 active:(bg-gray-400/20)" type="radio" name="activePreset" bind:group={activePreset} value={i}>
+					<span class="text">{preset.name}</span>
+				</div>
+			{/each}
 		</div>
-		<h1 class="text text-center">Reverb</h1>
-		<div class="flex flex-row children:min-w-1/2">
-			<div class="flex flex-col">
-			<h2 class="text">Decay: {properties.reverbDecay}</h2>
-			<input class="m-2" type="range" min="0.01" max="10" step="0.10" {disabled} bind:value={properties.reverbDecay} on:change={() => {updateValue("reverbDecay", properties.reverbDecay)}}>
+		<div class="min-w-[50%] mr-4 children:(flex flex-col justify-end items-center m-2 rounded-md bg-green-300/20)">
+			<div class={(properties.pitchWet > 0 ? "bg-green-300/20" : "passive-bg")} use:draggable>
+				<h1 class="text big-text">Pitch</h1>
+				<h2 class="text">Semitone Shift: {properties.pitch}</h2>
+				<input class="" type="range" min="-12" max="12" step="1" {disabled} bind:value={properties.pitch} on:change={() => {updateValue("pitch", properties.pitch)}}>
+				<h2 class="text">Active amount: {properties.pitchWet}</h2>
+				<input class="mb-2" type="range" min="0" max="1" step="0.01" {disabled} bind:value={properties.pitchWet} on:change={() => {updateValue("pitchWet", properties.pitchWet)}}>
 			</div>
-			<div class="flex flex-col">
-				<h2 class="text">Wetness: {properties.reverbWet}</h2>
-				<input class="m-2" type="range" min="0" max="1" step="0.01" {disabled} bind:value={properties.reverbWet} on:change={() => {updateValue("reverbWet", properties.reverbWet)}}>
+			<div class={(properties.reverbWet > 0 ? "bg-green-300/20" : "passive-bg")} use:draggable>
+				<h1 class="text-center text big-text">Reverb</h1>
+				<h2 class="text">Decay: {properties.reverbDecay}</h2>
+				<input class="mb-2" type="range" min="0.01" max="10" step="0.10" {disabled} bind:value={properties.reverbDecay} on:change={() => {updateValue("reverbDecay", properties.reverbDecay)}}>
+				<h2 class="text">Active amount: {properties.reverbWet}</h2>
+				<input class="mb-2" type="range" min="0" max="1" step="0.01" {disabled} bind:value={properties.reverbWet} on:change={() => {updateValue("reverbWet", properties.reverbWet)}}>
 			</div>
-		</div>
-		<h1 class="text">Volume: {properties.volume}</h1>
-		<input class="m-2" type="range" min="0" max="1" step="0.01" {disabled} bind:value={properties.volume} on:change={() => {updateValue("volume", properties.volume)}}>
-		<button class="p-2 m-2 bg-red-200" {disabled} on:click={() => {sendMessage({message: "toggle-media"}, "Played/paused.")}}>Play/Pause</button>
+			<div class="flex flex-col items-center justify-end m-2 rounded-md bg-green-300/20" use:draggable>
+				<h1 class="text big-text">Media Settings</h1>
+				<h2 class="text">Volume: {properties.volume}</h2>
+				<input class="mb-2" type="range" min="0" max="1" step="0.01" {disabled} bind:value={properties.volume} on:change={() => {updateValue("volume", properties.volume)}}>
+				<h2 class="text">Playback rate: {properties.playbackRate}x</h2>
+				<input class="mb-2" type="range" min="0" max="2" step="0.05" {disabled} bind:value={properties.playbackRate} on:change={() => {updateValue("playbackRate", properties.playbackRate)}}>
+				<button class="w-24 p-2 m-2 bg-red-200 rounded-sm active:(ring-4 ring-light-200/25)" {disabled} on:click={() => {sendMessage({message: "toggle-media"}, "Played/paused.")}}>Play/Pause</button>
+			</div>
 		</div>
 	</div>
-	<div class="flex flex-row pt-5 pl-3">
-	<p class="text">{status}</p>
-	<button class="w-24 p-2 m-2 bg-red-200" on:click={exitOptions}>Quit</button>
+	<div class="flex flex-row justify-between pt-5 pl-3">
+		<p class="text">{status}</p>
+		<button class="w-24 p-2 m-2 bg-red-200 rounded-sm active:(ring-4 ring-light-200/25)" on:click={exitOptions}>Quit</button>
 	</div>
 </div>
 
 <style>
 	.text {
-		@apply text-sm text-light-600 p-2
+		@apply text-sm text-light-600 p-2;
+	}
+
+	.big-text {
+		@apply text-lg;
+	}
+
+	.passive-bg {
+		@apply bg-green-300/5;
 	}
 
 
