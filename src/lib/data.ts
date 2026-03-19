4cf15ea89cb680fd05538b74f83bdff44badc438
Sonny <sonny0dev@gmail.com>
2026-03-19 20:36:32 +0000

    move i18n, simpylify types

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,61 +1,15 @@
-import type { Preset } from './types'
-
-function genProperties(playbackRate: number, pitch: number, pitchWet: number, reverbDecay: number, reverbWet: number) {
-    return { playbackRate, pitch, pitchWet, reverbDecay, reverbWet }
-};
+import type { Preset, PresetProperties } from './types'
 
 export const ERROR_VOLUME = 0.6767676767
 export const ERROR_PLAYBACK_RATE = 1
 
-export enum MESSAGES {
-    STATUS_WAITING = 'Waiting for media.',
-    STATUS_PLAYING = 'Playing.',
-    STATUS_TOGGLE_PLAYBACK = 'Played/paused.',
-    STATUS_ALREADY_PLAYING = 'Already playing.',
-    STATUS_FAILED_COMMAND = 'Failed, check devtools.',
-    STATUS_PRESETS_EMPTY = 'Couldn\'t get presets, reset to default.',
-    STATUS_EXIT = 'Exiting now.',
-    STATUS_QUIT = 'Quit connection.',
-    STATUS_NO_AUDIO_CONTENT = 'No audio detected on current tab.\nYou must play audio before opening the extension.',
-    STATUS_RECORD_FAILED = 'Error: unable to create and record from tab, check devtools.',
-    CONTENT_EXECUTED = 'Audio Mixer CONTENT: executed.',
-    NO_MEDIA = 'Error: no media found, despite the content tab playing audio.',
-    NO_MEDIA_PLAYING = 'Error: no playing media found.',
-    CAPTURE_ERROR = 'Error: record tab isn\'t playing audio but content tab is.',
-    CAPTURE_TAB_ERROR = 'Error: Unable to capture tab.',
-    CONTENT_MULTIPLE = 'Error: multiple media elements playing, unable to pick one.',
-    QUERY_MULTIPLE = 'Error: expected one element but got many, check query',
-    QUERY_NONE = 'Error: expected one element but got none, check query',
-    QUERY_UNABLE_SET_ATTRIBUTE = 'Error: unable to set attribute',
-    CREATE_TAB = 'Error: unable to create record tab.',
-    GET_TAB = 'Error: tab does not exist.',
-    REMOVE_TAB = 'Error: unable to remove tab.',
-    SET_VALUE = 'Error: unable to set value',
-    COMMAND_FAILED = 'Error: unable to send command',
-    EXITING = 'Exiting, closed tabs, cleared storage and reset playback rate.',
-    GITHUB_ISSUE = 'Please open an issue on GitHub with any details you have.',
-    GITHUB_WEBSITE = 'If this website is already supported please open an issue on GitHub, if it isn\'t supported yet feel free to message me or open an issue on GitHub.',
+function genPreset(name: string, user: boolean, properties: PresetProperties) {
+    return { name, user, properties }
 }
 
 export const DEFAULT_PRESETS: Preset[] = [
-    {
-        name: 'Default/Reset',
-        values: genProperties(1, 0, 0, 0.01, 0),
-    },
-    {
-        name: 'Classic Nightcore',
-        values: genProperties(1.25, 3, 1, 0.01, 0),
-    },
-    {
-        name: '(slowed + reverb)',
-        values: genProperties(0.75, 0, 0, 1, 0.7),
-    },
-    {
-        name: 'Roomy Reverb',
-        values: genProperties(1, 0, 0, 1.5, 0.8),
-    },
-    {
-        name: 'Custom',
-        values: {},
-    },
+    genPreset('Default/Reset', false, { playbackRate: 1, pitch: 0, pitchWet: 0, reverbDecay: 0.01, reverbWet: 0 }),
+    genPreset('Classic Nightcore', false, { playbackRate: 1.25, pitch: 3, pitchWet: 1, reverbDecay: 0.01, reverbWet: 0 }),
+    genPreset('(slowed + reverb)', false, { playbackRate: 0.75, pitch: 0, pitchWet: 0, reverbDecay: 1, reverbWet: 0.7 }),
+    genPreset('Roomy Reverb', false, { playbackRate: 1, pitch: 0, pitchWet: 0, reverbDecay: 1.5, reverbWet: 0.8 }),
 ]
--- src/lib/types.d.ts
+++ src/lib/types.d.ts
@@ -1,16 +1,11 @@
-export type ContentProperty = 'volume' | 'playbackRate'
+export type MediaProperty = 'volume' | 'playbackRate'
 export type ToneProperty = 'pitch' | 'pitchWet' | 'reverbDecay' | 'reverbWet'
-export type Property = ContentProperty | ToneProperty
-export type PropertyValue = number
 
-export interface OptionalProperties {
-    volume?: number
-    playbackRate?: number
-    pitch?: number
-    pitchWet?: number
-    reverbDecay?: number
-    reverbWet?: number
+export interface PropertyPayload<T> {
+    property: T
+    value: number
 }
+
 export interface PresetProperties {
     playbackRate: number
     pitch: number
@@ -18,57 +13,9 @@ export interface PresetProperties {
     reverbDecay: number
     reverbWet: number
 }
-export type PropertiesNoVolume = PresetProperties
-export type Properties = PresetProperties & {
-    volume: number
-}
+
 export interface Preset {
     name: string
-    values: PresetProperties | object
+    user: boolean
+    properties: PresetProperties
 }
-export type StartMixerResponse = Properties & {
-    message?: string
-}
-export interface AnyResponse {
-    [key: string]: string | number
-}
-
-export interface CustomEventData {
-    detail?: {
-        type: Property
-        value?: PropertyValue
-    }
-}
-export type GiveValueEvent = Event & {
-    detail: {
-        type: Property
-        value: PropertyValue
-    }
-}
-
-interface PopUpCommand {
-    command: 'SET_VALUE' | 'TOGGLE_PLAYBACK' | 'EXIT_MIXER'
-}
-export type PopUpCommands = PopUpCommand | {
-    popUpCommand
-    type: Property
-    value: number
-}
-export interface PageChange {
-    command: 'PAGE_CHANGE'
-    volume: number
-    playbackRate: number
-}
-export type ContentCommand = PageChange | {
-    command: 'GET_VALUE' | 'SET_VALUE' | 'TOGGLE_PLAYBACK' | 'PING'
-    type?: ContentProperty
-    value?: number
-}
-
-interface Custom {
-    playing: boolean
-};
-export type MediaElement = HTMLAudioElement | HTMLVideoElement
-export type MediaElements = MediaElement[]
-export type NullMedia = MediaElement | null
-export type CustomMedia = (HTMLAudioElement & Custom)|(HTMLVideoElement & Custom)
