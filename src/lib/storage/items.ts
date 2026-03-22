0db7ca56161dc47b397f0982459081d02032757a
Sonny <sonny0dev@gmail.com>
2026-03-22 22:43:40 +0000

    move defaults to items waow

--- src/lib/storage/items.ts
+++ src/lib/storage/items.ts
@@ -1,6 +1,12 @@
 import type { Preset } from 'lib/types'
 import { storage } from '#imports'
-import { DEFAULT_PRESETS } from 'lib/data'
+
+const DEFAULT_PRESETS: Preset[] = [
+    { name: 'Default/Reset', user: false, properties: { playbackRate: 1, pitch: 0, pitchWet: 0, reverbDecay: 0.01, reverbWet: 0 } },
+    { name: 'Classic Nightcore', user: false, properties: { playbackRate: 1.25, pitch: 3, pitchWet: 1, reverbDecay: 0.01, reverbWet: 0 } },
+    { name: '(slowed + reverb)', user: false, properties: { playbackRate: 0.75, pitch: 0, pitchWet: 0, reverbDecay: 1, reverbWet: 0.7 } },
+    { name: 'Roomy Reverb', user: false, properties: { playbackRate: 1, pitch: 0, pitchWet: 0, reverbDecay: 1.5, reverbWet: 0.8 } },
+]
 
 export const volume = storage.defineItem<number>('session:volume', { fallback: 0.06767 })
 export const playbackRate = storage.defineItem<number>('session:playbackRate', { fallback: 1 })
@@ -9,7 +15,6 @@ export const pitchWet = storage.defineItem<number>('session:pitchWet', { fallbac
 export const reverbDecay = storage.defineItem<number>('session:reverbDecay', { fallback: 0.01 })
 export const reverbWet = storage.defineItem<number>('session:reverbWet', { fallback: 0 })
 
-// popup
 export const presets = storage.defineItem<Preset[]>('local:presets', { fallback: DEFAULT_PRESETS })
 export const togglePlayback = storage.defineItem<boolean>('session:togglePlayback', { fallback: false })
 
